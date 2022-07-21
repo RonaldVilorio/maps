7a725d86cfa1dfcae58fa16e8eefde872961ae2e
RonaldVilorio <ronvilorio@gmail.com>
2022-07-21 14:11:12 -0400

    working on rendering map

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { User } from "./User";
 import {Company} from "./Company"
 
-new google.maps.Map(document.getElementById('map'),{
-   zoom:1,
-   center:{
-    lat:0,
-    lng:0
-   } 
-});
+// new google.maps.Map(document.getElementById('map'),{
+//    zoom:1,
+//    center:{
+//     lat:0,
+//     lng:0
+//    } 
+// });
